224c474265cbe8fbeaa993f2f9dd2b25e4781f93
Ira Tsarikova <tsar0911@gmail.com>
2016-06-12 13:54:57 +0300

    yandex translator java api and activity for dictionary
    was added library json_simple-1.1.jar and activity_dictionary

---
+++ app/src/main/java/com/example/tsar/booksherlockholmes/Data/DatabaseHelper.java
@@ -0,0 +1,49 @@
+package com.example.tsar.booksherlockholmes.Data;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by tsar on 22.12.2015.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static String DATABASE_NAME = "SherlockHolmesDB.db";
+    private static final String TEXT = "text";
+    private static final String ID = "id";
+    private static final String TABLE_NAME = "ScandalInBohemia";
+
+    public DatabaseHelper(Context context){
+        super(context,DATABASE_NAME, null, 1);
+    }
+    
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public String getText(String id){
+        Log.d("DB", "START TO GET FRAGMENT");
+        SQLiteDatabase db = this.getReadableDatabase();
+        String out = null;
+        Log.d("DB", "TRY TO GET FRAGMENT");
+        Cursor cursor = db.query(TABLE_NAME, new String[]{ID,TEXT}, ID + "=?",
+                new String[]{id}, null, null, null, null);
+        if (cursor != null && cursor.getCount() != 0) {
+            cursor.moveToFirst();
+            Log.d("DB", "TRY TO GET STRING");
+            out = cursor.getString(1);
+            //return cursor.getString(1);
+        }
+        cursor.close();
+        return out;
+    }
+}
---
+++ app/src/main/java/com/example/tsar/booksherlockholmes/MyMediaPlayer.java
@@ -0,0 +1,91 @@
+package com.example.tsar.booksherlockholmes;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+/**
+ * Created by tsar on 24.12.2015.
+ */
+public class MyMediaPlayer {
+    private MediaPlayer mediaPlayer;
+    private double startTime = 0;
+    private double finalTime = 0;
+    private android.os.Handler durationHandler = new android.os.Handler();
+    private SeekBar seekBar;
+    private static int oneTimeOnly = 0;
+    private ImageView play, pause;
+
+    public MyMediaPlayer(Context context, SeekBar sb, ImageView bt_play, ImageView bt_pause) {
+        mediaPlayer = MediaPlayer.create(context, R.raw.theadventuresofsherlockholmes001adventure1ascandalinbohemia);
+        seekBar = sb;
+        play = bt_play;
+        pause = bt_pause;
+        finalTime = mediaPlayer.getDuration();
+        seekBar.setMax((int) finalTime);
+    }
+
+    public void play() {
+        mediaPlayer.start();
+        startTime = mediaPlayer.getCurrentPosition();
+
+        seekBar.setProgress((int) startTime);
+        durationHandler.postDelayed(UpdateSongTime, 1000);
+
+        play.setVisibility(View.GONE);
+        pause.setVisibility(View.VISIBLE);
+    }
+
+    public void pause() {
+        try {
+        mediaPlayer.pause();
+       //     mediaPlayer.release();
+        pause.setVisibility(View.GONE);
+        play.setVisibility(View.VISIBLE);}
+        catch(Exception e){}
+    }
+
+    public void activity_pause(){
+        int time = mediaPlayer.getCurrentPosition();
+        if(time != 0){
+        startTime = time;
+        pause();}
+
+
+    }
+    public void activity_resume(){
+        mediaPlayer.seekTo((int) startTime);
+
+
+    }
+    public void activity_saveIS(Bundle outState){
+        outState.putInt("possition", mediaPlayer.getCurrentPosition());
+        mediaPlayer.pause();
+
+    }
+    public void activity_RestoreIS(Bundle savedInstanceState){
+        int pos = savedInstanceState.getInt("possition");
+        if(pos != 0)
+        seekBarChange(true,pos);
+    }
+
+    private Runnable UpdateSongTime = new Runnable() {
+        public void run() {
+            startTime = mediaPlayer.getCurrentPosition();
+            seekBar.setProgress((int) startTime);
+            durationHandler.postDelayed(this, 100);
+        }
+    };
+
+    public void seekBarChange(Boolean fromUser, int progress){
+        if (fromUser) {
+            mediaPlayer.seekTo(progress);
+            seekBar.setProgress(progress);
+        }
+    }
+
+
+}
