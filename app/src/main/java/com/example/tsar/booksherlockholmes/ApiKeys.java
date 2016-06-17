f67efb43a9d6f8ba5650372a73bfa54055f14969
Tsar09 <tsar0911@gmail.com>
2016-06-17 10:08:07 +0300

    Update ApiKeys.java

--- app/src/main/java/com/example/tsar/booksherlockholmes/ApiKeys.java
+++ app/src/main/java/com/example/tsar/booksherlockholmes/ApiKeys.java
@@ -4,5 +4,5 @@ package com.example.tsar.booksherlockholmes;
  * Created by адинистр on 16.06.2016.
  */
 public class ApiKeys {
-    protected static final String YANDEX_API_KEY = "trnsl.1.1.20160611T120716Z.86f5aee787936cc9.d0052bfb64dffe7fe22db47e8b5e4402fb9ab8bd";
+    protected static final String YANDEX_API_KEY = "api";
 }
